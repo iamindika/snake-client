eb8a884a0c190d29710171cbcc6a050a21616bbd
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2020-11-05 05:04:02 +0000

    Moved the initial input functions into input.js and created exports/imports for play.js

--- play.js
+++ play.js
@@ -1,30 +1,9 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
 console.log('Connecting ...');
 connect();
 
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function(cb) {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  
-  return stdin;
-}
-
-const handleUserInput = key => {
-  process.stdout.write('.');
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-
-setupInput(handleUserInput);
+setupInput();
 
 
