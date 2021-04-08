dc7780d45fb391877e3689a407d373b5511f488f
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2021-04-08 17:02:56 +0000

    Restart project to DRY up code
    Also for more practice

--- play.js
+++ play.js
@@ -1,8 +1,28 @@
-const { connect } = require('./client');
-const { setupInput } = require('./input');
+// const { connect } = require('./client');
+// const { setupInput } = require('./input');
 
-console.log('Connecting ...');
+// console.log('Connecting ...');
 
-setupInput(connect());
+// setupInput(connect());
 
+const net = require('net');
 
+const connect = () => {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+    // host: "135.23.222.131",
+    // port: 50542
+  })
+
+  conn.setEncoding('utf-8');
+
+  conn.on('data', (data) => {
+    console.log(data);
+  })
+
+  return conn;
+};
+
+console.log('Connecting...')
+connect();
