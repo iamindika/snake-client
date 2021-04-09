e528c39f2665d41bfd6809344a87fc6d3bb25fa5
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2021-04-09 18:41:43 +0000

    DRY play.js by moving input function handlers to input.js

--- input.js
+++ input.js
@@ -1,46 +1,48 @@
-const { MOVE_UP_KEY, MOVE_DOWN_KEY, MOVE_LEFT_KEY, MOVE_RIGHT_KEY, MESSAGES } = require('./constants');
-let connection;
+// const { MOVE_UP_KEY, MOVE_DOWN_KEY, MOVE_LEFT_KEY, MOVE_RIGHT_KEY, MESSAGES } = require('./constants');
+// let connection;
 
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function(conn) {
-  connection = conn;
+const setupInput = () => {
   const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
+  stdin.setRawMode('true');
+  stdin.setEncoding('utf-8');
   stdin.resume();
-  stdin.on('data', handleUserInput);
-  
+
+  handleUserInput(stdin);
+
   return stdin;
 }
 
-const handleUserInput = key => {
+const handleUserInput = inputConn => {
   //process.stdout.write('.');
-  if (key === '\u0003') {
-    process.exit();
-  }
+  inputConn.on('data', (key) => {
+    if(key === '\u0003') {
+      process.exit();
+    }
+  })
 
-  if (key === 'w') {
-    connection.write(MOVE_UP_KEY);
-  }
+  // if (key === 'w') {
+  //   connection.write(MOVE_UP_KEY);
+  // }
 
-  if (key === 's') {
-    connection.write(MOVE_DOWN_KEY);
-  }
+  // if (key === 's') {
+  //   connection.write(MOVE_DOWN_KEY);
+  // }
   
-  if (key === 'a') {
-    connection.write(MOVE_LEFT_KEY);
-  }
+  // if (key === 'a') {
+  //   connection.write(MOVE_LEFT_KEY);
+  // }
   
-  if (key === 'd') {
-    connection.write(MOVE_RIGHT_KEY);
-  }
+  // if (key === 'd') {
+  //   connection.write(MOVE_RIGHT_KEY);
+  // }
 
-  if (MESSAGES[key]) {
-    connection.write(MESSAGES[key]);
-  }
+  // if (MESSAGES[key]) {
+  //   connection.write(MESSAGES[key]);
+  // }
 };
 
 module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,28 +1,12 @@
 const { connect } = require('./client');
-// const { setupInput } = require('./input');
+const { setupInput } = require('./input');
 
 // console.log('Connecting ...');
 
 // setupInput(connect());
-const handleUserInput = (inputConn) => {
-  inputConn.on('data', (key) => {
-    if(key === '\u0003') {
-      process.exit();
-    }
-  })
-}
-
-const setupInput = () => {
-  const stdin = process.stdin;
-  stdin.setRawMode('true');
-  stdin.setEncoding('utf-8');
-  stdin.resume();
-
-  handleUserInput(stdin);
-
-  return stdin;
-}
 
 console.log('Connecting...')
+
 connect();
+
 setupInput();
